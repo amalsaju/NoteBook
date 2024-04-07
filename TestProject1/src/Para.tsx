556a7075bba661d97060ede536ca7792fb3eb0db
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2024-04-07 21:55:11 +1000

    Initiliased React

---
+++ TestProject1/src/Para.tsx
@@ -0,0 +1,12 @@
+
+
+const Para = () => {
+    return(
+        <>
+        <p>Hello this is a test paragraph!</p>
+        </>
+    );
+}
+
+
+export default Para;
---
+++ TestProject1/src/app.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+import Para from './Para';
+
+const root = createRoot(document.body);
+root.render(<Para />);
