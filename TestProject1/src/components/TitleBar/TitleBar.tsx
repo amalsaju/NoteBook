e8ed41a172c1f3e32c97dfbd1ae80cb4ee70f437
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2024-04-21 21:16:24 +1000

    Added dragging to app

--- TestProject1/src/components/TitleBar/TitleBar.tsx
+++ TestProject1/src/components/TitleBar/TitleBar.tsx
@@ -7,7 +7,7 @@ const TitleBar = () => {
     return (
         <div className="flex bg-gray-800 max-w-7xl 
         border-b-slate-500 
-        border-b-2">
+        border-b-2 draggable">
             <MenuFunctions className="w-1/3 flex" />
             <span className=" w-1/3 block-inline">NoteBook</span>
             <WindowFunctions className="w-1/3 self-end" />
