a0041e7afdf13b4241155d378050f22c5f6a0e1f
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2024-04-29 19:31:56 +1000

    Added Jotai for state management

--- TestProject1/src/components/Markdown.tsx
+++ TestProject1/src/components/Markdown.tsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 
-import { MDXEditor, MDXEditorMethods, listsPlugin } from '@mdxeditor/editor'
+import { MDXEditor, MDXEditorMethods, listsPlugin, markdown$, markdownShortcutPlugin } from '@mdxeditor/editor'
 import { headingsPlugin } from '@mdxeditor/editor'
 
 import '@mdxeditor/editor/style.css'
@@ -9,26 +9,42 @@ import React from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 import path from 'path';
 import { fileLoadKey, fileSaveKey } from '../shared/settings';
+import { useAtom } from 'jotai';
+import { markdownValueStore } from '../store/store';
 
 
 const MyMarkdown = () => {
 
-  const [markdownValue, setMarkdownValue] = useState('sdkfljsldkjflksjfklsjdlfjslkdfjsjdklfjlsdjflkjsdlkfsdjfksdjfljsdlfjlksdjflksdjflkjsdkfjskdljfklsjdflkjsdlkfjklsdjflkjsdklfjskljdfklsjdfljsdklfjskldjflksjdflkjsdkjflsdjflksdjflkjsdlkfjsdlkfjlksdjflksdjflksdjfkjsdklfjslkdjfklsdjflksdjlfkjsdlkfjlksdjflksdf');
-  const ref = React.useRef<MDXEditorMethods>(null);
+  // const [markdownValue, setMarkdownValue] = useAtom(markdownValueStore);
+  // const ref = React.useRef<MDXEditorMethods>(null);
 
-  useHotkeys(fileSaveKey, () => window.electronAPI.onFileSave("Hellow world!"));
-  useHotkeys(fileLoadKey, () => {
-    const result:string = window.electronAPI.onFileLoad();
-    console.log("the result received is:" + result);
-    setMarkdownValue(result);
+  // useHotkeys(fileSaveKey, () => {
+  //   console.log("Save key pressed!");
+  //   window.electronAPI.onFileSave("Hellow world!");
+  // });
+  // useHotkeys(fileLoadKey, () => {
+  //   console.log("Open key pressed!");
+  //   const result:string = window.electronAPI.onFileLoad();
+  //   console.log("the result received is:" + result);
+  //   setMarkdownValue(result);
+  //   console.log("Markdown value is" + markdownValue);
+  // });
 
-  });
+  // todo
+  // Create a file class having info such as title, content, id, etc. 
+  // save to the file on change
+  // in the markdown props provide the content value
+  // this is usefull when we want to change/re-render the entire content
+  // you don't have to handle the value changing while you are editing the document
+  // its done automatically
+
+  // move the short cut key somewhere else
 
   return (
     <div className='block mr-3'>
-      <MDXEditor ref={ref} markdown={markdownValue} plugins={[headingsPlugin(), listsPlugin()]}
-        onChange={() => setMarkdownValue(ref.current?.getMarkdown())}
-        contentEditableClassName='prose prose-p:leading-normal prose-p:m-0 max-w-none' autoFocus />
+      <MDXEditor markdown={'hello'} plugins={[headingsPlugin(), listsPlugin(), markdownShortcutPlugin()]}
+        contentEditableClassName='prose prose-p:leading-normal prose-p:m-0 
+        max-w-none' autoFocus />
     </div>
   );
 
---
+++ TestProject1/src/store/store.ts
@@ -0,0 +1,3 @@
+import { atom, useAtom, useSetAtom } from 'jotai';
+
+export const markdownValueStore = atom('## Generic welcome text');
