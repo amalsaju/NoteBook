89f417135b5b14c41ee8d778726f85a9538bc03b
Amal Saju Kalarickal <amalsaju1998@gmail.com>
2024-05-15 20:21:57 +1000

    Added styling to tabs

--- TestProject1/src/components/EditableTitle.tsx
+++ TestProject1/src/components/EditableTitle.tsx
@@ -1,4 +1,4 @@
-import { useAtom, useAtomValue } from 'jotai';
+import { atom, useAtom, useAtomValue } from 'jotai';
 import { selectedFileAtom } from '../store/store';
 import { useState } from 'react';
 
@@ -7,20 +7,24 @@ export interface EditableTitleProps {
     title: string;
 }
 
+const clickedAtom = atom(false);
+
 const EditableTitle = ({ classname, title }: EditableTitleProps) => {
 
 
-    const [clicked, setClicked] = useState(false);
+    const [clicked, setClicked] = useAtom(clickedAtom);
 
     const [selectedFile, setSelectedFile] = useAtom(selectedFileAtom);
+
     return (
         <div className={classname}>
-            {clicked ? <button className='w-28' onClick={() => setClicked(!clicked)}>{title + '.md'}</button> :
+            {clicked ?
                 <input className='text-black w-28 text-center' value={title}
                     onChange={e => setSelectedFile((selectedFile) =>
                         ({ ...selectedFile, name: e.target.value }))}
                     onBlur={() => setClicked(!clicked)}
-                    onKeyDown={e => e.key === 'Enter' && setClicked(!clicked)} autoFocus />}
+                    onKeyDown={e => e.key === 'Enter' && setClicked(!clicked)} autoFocus /> :
+                <button className='w-28' onClick={() => setClicked(!clicked)}>{title + '.md'}</button>}
         </div>
     );
 
--- TestProject1/src/components/TabBar.tsx
+++ TestProject1/src/components/TabBar.tsx
@@ -11,10 +11,10 @@ const TabBar = () => {
     const selectedFile = useAtomValue(selectedFileAtom);
 
     return (
-        <div className="flex bg-gray-800 max-w-none text-white">
+        <div className="flex bg-zinc-900 max-w-none text-white">
             {files.map((file) => (
-                <EditableTitle title={file.name} 
-                classname={selectedFile.name == file.name ? "ml-0.5 border-t-2 border-cyan-400" : "ml-0.5" } />
+                <EditableTitle key={file.uid} title={file.name}
+                    classname={selectedFile.name == file.name ? "ml-0.5 bg-zinc-700 border-r-2 border-x-neutral-400 border-t-2 border-cyan-400" : "ml-0.5 border-x-neutral-400 border-r-2"} />
             ))}
         </div>
     );
